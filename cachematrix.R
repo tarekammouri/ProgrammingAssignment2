111c6463e1c5190d1db0203db71526b9b84eefcf
Tarek Ammouri <tarekamm1998@gmail.com>
2020-07-07 10:47:31 +0300

    Code by Tarek Ammouri on July 7th, 2020

--- cachematrix.R
+++ cachematrix.R
@@ -10,7 +10,7 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setinverse <- function(solve) m <<- solve
+  setinverse <- function(inverse) m <<- inverse
   getinverse <- function() m
   list(set = set, get = get,
        setinverse = setinverse,
